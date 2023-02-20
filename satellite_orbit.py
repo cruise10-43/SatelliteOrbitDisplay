71d1594b7f8ddf34a25cb068bafb24935df607f6
cruise10-43 <space.earth.10.katou@gmail.com>
2023-02-20 23:30:27 +0900

    Update:satellite_orbit.py  error occurred!

--- satellite_orbit.py
+++ satellite_orbit.py
@@ -1,9 +1,12 @@
 import skyfield.api
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
 import numpy as np
 import datetime
 
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
 
 def get_sat_tle_info():
     sat_list = skyfield.api.load.tle_file('https://celestrak.org/NORAD/elements/gnss.txt', reload=1)
@@ -11,13 +14,7 @@ def get_sat_tle_info():
     return sat_list
 
 
-if __name__ == '__main__':
-    print('satellite_orbit')
-
-    # 描画エリアの作成
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-
+def update():
     sats = get_sat_tle_info()
     ts = skyfield.api.load.timescale()
     sat = sats[0]
@@ -29,8 +26,7 @@ if __name__ == '__main__':
 
     minutes = 0
     base_time = datetime.datetime(2023, 2, 19, 12, 0, 0, 000)
-    for num in range(300):
-
+    for num in range(100):
         edit_time = ts.tt(base_time.year, base_time.month, base_time.day,
                           base_time.hour, base_time.minute, base_time.second)
         print(sat.at(edit_time).position.km)
@@ -40,6 +36,16 @@ if __name__ == '__main__':
         # 5minutes add
         base_time = base_time + datetime.timedelta(hours=5)
 
-    ax.scatter(x, y, z, s=10, c='red')
+    ax.scatter(x, y, z, s=1, c='red')
+
+
+if __name__ == '__main__':
+    print('satellite_orbit')
+
+    # 描画エリアの作成
+
+    anim = FuncAnimation(fig, update())
+
+
     # 描画
     plt.show()
